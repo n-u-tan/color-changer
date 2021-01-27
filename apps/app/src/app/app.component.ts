d1d1f8351f205536c8686d8133331a4518faa6d9
Nousagi Tangent <nousagi.tangent@gmail.com>
2021-01-27 23:04:42 +0800

    Add input and canvas, make basic input image show

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'color-changer-root',
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  @ViewChild('fileInput', { static: true }) public fileInputElement: ElementRef<
+    HTMLInputElement
+  >;
+  @ViewChild('canvas', { static: true }) public canvasElement: ElementRef<
+    HTMLCanvasElement
+  >;
+
+  public onFileChanged() {
+    const file: Blob = this.fileInputElement.nativeElement.files[0];
+    const fr = new FileReader();
+    fr.onload = () => {
+      this.createImage(fr);
+    };
+    fr.readAsDataURL(file);
+  }
+
+  private createImage(fr: FileReader) {
+    const img = new Image();
+    img.onload = () => {
+      this.imageLoaded(img);
+    };
+    img.src = fr.result as string;
+  }
+
+  private imageLoaded(img: HTMLImageElement) {
+    const canvas = this.canvasElement.nativeElement;
+    canvas.width = img.width; // set canvas size big enough for the image
+    canvas.height = img.height;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0); // draw the image
+  }
 }
